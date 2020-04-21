8784b1919d06968811f7d8801dbf82e31c680c4b
tugooner1999 <63830419+tugooner1999@users.noreply.github.com>
2020-04-21 21:51:31 +0700

    Create truu-tuong1.php

---
+++ truu-tuong1.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class Arsenal{
+	var $hlv_truong;
+	var $hlv_thumon;
+	var $hlv_hauve;
+	var $hlv_tiendao;
+	var $troly_hlv;
+	var $cauthu;
+	var $stadium;
+	var $nhanvien_aodau;
+
+
+	function trandau(){
+
+	}
+}
+
+
+
+class LopHoc{
+	var $tenSV;
+	var $tenGV;
+	var $tenLop;
+	var $tenTruong;
+	var $soSV;
+	var $maSV;
+	var $sdt;
+	var $email;
+
+
+	function diHoc(){
+		
+	}
+}
+
+
+
+
+?>
